62ed96837cdb8e0d4fae1541f0202a465ab77e2f
bgwinston <b.winston87@yahoo.com>
2021-06-21 22:09:10 -0500

    Cleaned existing code.

--- src/main/java/org/launchcode/cheesemvc/Controllers/cheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/Controllers/cheeseController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 
 @Controller
 @RequestMapping("cheese")
